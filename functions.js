f73f4503bc83454a424b6ef9dc872658abfc12b0
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-06-06 23:35:39 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,15 @@
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {
+}
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+addTwo(5);
+getUpper("pragati");
+signUpUser("pragati", "pragati@gmail.com", false);
+loginUser("pragati", "pragati@gmail.com");
---
+++ functions.ts
@@ -0,0 +1,17 @@
+function addTwo(num: number){
+  return num+2;
+}
+
+function getUpper(val: string){
+  return val.toUpperCase();
+}
+function signUpUser(name: string, email: string, isPaid: boolean){
+
+}
+
+let loginUser=(name: string, email: string, isPaid: boolean = false)=>{}
+
+addTwo(5);
+getUpper("pragati");
+signUpUser("pragati", "pragati@gmail.com", false);
+loginUser("pragati", "pragati@gmail.com");
