6214ae11760cc93bad28b1e9bcb00ad8b64fffca
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-06-11 23:51:13 +0530

    pure ts setup

--- pure ts/src/index.ts
+++ pure ts/src/index.ts
@@ -1,1 +1,10 @@
-console.log("typescript is here")
+class User {
+  email: string
+  name: string
+  constructor(email: string,name: string){
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const pragati= new User("pragati@gmail.com", "pragati")
