c28d26624e4f30a09f0161b431819745c72da7bf
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-06-22 23:28:47 +0530

    modified keywords, public &pvt

--- pure ts/dist/index.js
+++ pure ts/dist/index.js
@@ -1,3 +1,18 @@
 "use strict";
-console.log("typescript is here");
-console.log("typescript is amazing");
+class User {
+    constructor(email, name) {
+        this.city = "Jaipur";
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+}
+const pragati = new User("pragati@gmail.com", "pragati");
+class NewUser {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "Jaipur";
+    }
+}
+const newpragati = new User("pragati@gmail.com", "pragati");
--- pure ts/src/index.ts
+++ pure ts/src/index.ts
@@ -1,10 +1,22 @@
 class User {
   email: string
   name: string
+  private readonly city:string = "Jaipur"
   constructor(email: string,name: string){
+    this.city="";
     this.email = email;
     this.name = name;
   }
 }
 
-const pragati= new User("pragati@gmail.com", "pragati")
+const pragati= new User("pragati@gmail.com", "pragati");
+
+class NewUser {
+  readonly city:string = "Jaipur"
+  constructor(
+    public email: string,
+    public name: string){
+  }
+}
+
+const newpragati= new User("pragati@gmail.com", "pragati");
