9a1e3a85690cc7d6c0026c3ee4dd436037c053e2
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-06-22 23:48:43 +0530

    getters and setters

--- pure ts/dist/index.js
+++ pure ts/dist/index.js
@@ -12,7 +12,23 @@ class NewUser {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Jaipur";
+    }
+    deleteToken() {
+        console.log("token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const newpragati = new User("pragati@gmail.com", "pragati");
--- pure ts/src/index.ts
+++ pure ts/src/index.ts
@@ -12,10 +12,30 @@ class User {
 const pragati= new User("pragati@gmail.com", "pragati");
 
 class NewUser {
+  private _courseCount = 1
   readonly city:string = "Jaipur"
   constructor(
     public email: string,
     public name: string){
+  }
+
+  private deleteToken(){
+    console.log("token deleted")
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`
+  }
+
+  get courseCount() : number{
+    return this._courseCount
+  }
+
+  set courseCount(courseNum) {
+    if(courseNum <= 1){
+            throw new Error("course count should be more than 1")
+        }
+        this._courseCount= courseNum;
   }
 }
 
